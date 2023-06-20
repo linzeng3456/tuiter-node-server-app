67adc7ca61048fa380c53d53d43b383bb815ab6a
Zeng <linzeng3456@gmail.com>
2023-06-20 15:48:16 -0700

    test model

--- controllers/tuits/tuits-dao.js
+++ controllers/tuits/tuits-dao.js
@@ -1,5 +1,10 @@
 import tuitsModel from './tuits-model.js';
-export const findTuits = () => tuitsModel.find();
+export const findTuits = () => {
+
+  console.log("----tuitsModel======")
+  console.log(tuitsModel)
+  return tuitsModel.find();
+}
 export const createTuit = (tuit) => tuitsModel.create(tuit);
 export const deleteTuit = (tid) => tuitsModel.deleteOne({_id: tid});
 export const updateTuit = (tid, tuit) => tuitsModel.updateOne({_id: tid}, {$set: tuit})
